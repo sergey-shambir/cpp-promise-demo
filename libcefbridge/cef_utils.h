34dd62efda339808431c43b65705aa376a214877
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2017-02-22 19:38:00 +0300

    Убрал неполные примеры для CEF3

--- libcefbridge/cef_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "include/cef_app.h"
-
-template <class T, class ...TArgs>
-CefRefPtr<T> MakeCefRef(TArgs&&... args)
-{
-    return CefRefPtr<T>(new T(std::forward<TArgs>(args)...));
-}
